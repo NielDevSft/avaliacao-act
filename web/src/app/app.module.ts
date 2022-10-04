edf438fb028885559c947491b2fde3b6b7ce0666
NielDevSft <85716027+NielDevSft@users.noreply.github.com>
2022-10-04 19:07:04 -0300

    check point

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -5,17 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from "@angular/material/card";
-import { MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS } from "@angular/material/form-field";
+import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatRadioModule } from '@angular/material/radio';
+import { MatTableModule } from '@angular/material/table';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatNativeDateModule } from '@angular/material/core';
+import { LancamentoTableComponent } from './components/lancamento-table/lancamento-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LancamentoTableComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +30,10 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatCardModule,
     MatInputModule,
     MatSelectModule,
+    MatGridListModule,
+    MatButtonModule,
     MatRadioModule,
+    MatTableModule,
     MatDatepickerModule,
     MatFormFieldModule,
     FlexLayoutModule
