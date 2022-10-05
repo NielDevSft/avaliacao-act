fe44b99652735fa5ada00b43403ce92a77fd2da7
jefferson1709 <jefferson1709@hotmail.com>
2022-10-04 21:53:56 -0300

    Alterando nome db

--- api/src/main/java/com/administrativo/controllers/LancamentoController.java
+++ api/src/main/java/com/administrativo/controllers/LancamentoController.java
@@ -1,7 +1,10 @@
-package com.administrativo;
+package com.administrativo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import com.administrativo.Lancamento;
+import com.administrativo.repositories.LancamentoRepository;
 
 import java.util.List;
 
@@ -13,12 +16,10 @@ public class LancamentoController {
     @Autowired
     LancamentoRepository lancamentoRepository;
 
-    @GetMapping("api/lancamento")
     public List<Lancamento> getAllLancamento(){
         return lancamentoRepository.findAll();
     }
     
-    @PostMapping("api/lancamento")
     public Lancamento saveLancamento(@RequestBody Lancamento lancamento){
         return lancamentoRepository.save(lancamento);
     }
--- api/src/main/java/com/administrativo/repositories/LancamentoRepository.java
+++ api/src/main/java/com/administrativo/repositories/LancamentoRepository.java
@@ -1,7 +1,9 @@
-package com.administrativo;
+package com.administrativo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.administrativo.Lancamento;
 
 @Repository
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
